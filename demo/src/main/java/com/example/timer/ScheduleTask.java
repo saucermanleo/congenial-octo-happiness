0de3f9623e72e70bd85c352169793e70ffc1ac96
zhangyang <38433673+saucermanleo@users.noreply.github.com>
2018-04-19 22:59:06 +0800

    基于jdk的Timer的定时器任务

---
+++ demo/src/main/java/com/example/timer/ScheduleTask.java
@@ -0,0 +1,41 @@
+package com.example.timer;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Zangy
+ * 定时任务
+ */
+@Component
+public class ScheduleTask implements InitializingBean {
+	
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/DD hh:mm:ss");
+		Calendar calendar =  Calendar.getInstance();
+		calendar.setTime(new Date());
+		/*calendar.set(Calendar.AM_PM, Calendar.PM);
+		calendar.set(Calendar.HOUR, 10);
+		calendar.set(Calendar.MINUTE,40);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.add(Calendar.DAY_OF_YEAR, 1);*/
+		calendar.add(Calendar.MINUTE, 1);
+		Date startime = calendar.getTime();
+		System.out.println(format.format(startime));
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+
+			@Override
+			public void run() {
+				System.out.println(format.format(new Date()));
+			}}, startime, 1000);
+	}
+
+}
