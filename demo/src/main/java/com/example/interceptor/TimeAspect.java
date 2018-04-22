0471ac8bd7102073d4c68da449f483b4d7841473
zhangyang <38433673+saucermanleo@users.noreply.github.com>
2018-04-22 15:17:53 +0800

    1

---
+++ demo/src/main/java/com/example/interceptor/TimeAspect.java
@@ -0,0 +1,27 @@
+package com.example.interceptor;
+
+import java.util.Date;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeAspect {
+	@Around(value = "execution(* com.example.controller..*(..))")
+	public Object around(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("time aspect start");
+		long start = new Date().getTime();
+		for (Object arg : pjp.getArgs()) {
+			System.out.println(arg.toString());
+		}
+		Object o = pjp.proceed();
+		long end = new Date().getTime() - start;
+		System.out.println(end);
+		System.out.println("time aspect finish");
+		return o;
+
+	}
+}
