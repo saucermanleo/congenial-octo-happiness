b97adb1a6967b50f2e681a37b9c4992cd4b78fce
zhangyang <38433673+saucermanleo@users.noreply.github.com>
2018-04-26 01:21:15 +0800

    2

--- demo/src/main/java/com/example/properties/MyProperties.java
+++ demo/src/main/java/com/example/properties/MyProperties.java
@@ -8,6 +8,16 @@ import org.springframework.stereotype.Component;
 public class MyProperties {
 	private String logingpage = "/logintest.html";
 	
+	private int remebermSecends = 3600*24*7;
+	
+	public int getRemebermSecends() {
+		return remebermSecends;
+	}
+
+	public void setRemebermSecends(int remebermSecends) {
+		this.remebermSecends = remebermSecends;
+	}
+
 	private ValidateCodeProperties vp = new ValidateCodeProperties();
 
 	public ValidateCodeProperties getVp() {
