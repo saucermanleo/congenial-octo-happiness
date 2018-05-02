95a5fb203fc71132830388a0f80ec4678a6afd10
zhangyang <38433673+saucermanleo@users.noreply.github.com>
2018-05-02 17:09:22 +0800

    create a project for study

---
+++ pmk/src/main/java/com/zy/pmk/PmkApplication.java
@@ -0,0 +1,12 @@
+package com.zy.pmk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmkApplication.class, args);
+	}
+}
