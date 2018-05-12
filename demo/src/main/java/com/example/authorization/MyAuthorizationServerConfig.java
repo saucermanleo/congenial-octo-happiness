ffa082945f1797385f45658a80f09d212a67c79a
zhangyang <38433673+saucermanleo@users.noreply.github.com>
2018-05-12 22:12:05 +0800

    1

--- demo/src/main/java/com/example/authorization/MyAuthorizationServerConfig.java
+++ demo/src/main/java/com/example/authorization/MyAuthorizationServerConfig.java
@@ -1,10 +1,36 @@
 package com.example.authorization;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
-public class MyAuthorizationServerConfig {
+public class MyAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	
 
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("test").secret("123456").accessTokenValiditySeconds(7200);
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.userDetailsService(userDetailsService)
+		.authenticationManager(authenticationManager);
+	}
+	
+	
 }
