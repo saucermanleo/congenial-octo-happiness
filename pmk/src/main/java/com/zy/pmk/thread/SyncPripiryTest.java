80ab13639f896b46b2d59a14dac0159d0585b6d5
zhangyang <38433673+saucermanleo@users.noreply.github.com>
2018-05-25 18:01:00 +0800

    1

---
+++ pmk/src/main/java/com/zy/pmk/thread/SyncPripiryTest.java
@@ -0,0 +1,31 @@
+package com.zy.pmk.thread;
+
+class MyThread1 extends Thread{  
+    public MyThread1(String name) {
+        super(name);
+    }
+
+    public void run(){
+        for (int i=0; i<5; i++) {
+            System.out.println(Thread.currentThread().getName()
+                    +"("+Thread.currentThread().getPriority()+ ")"
+                    +", loop "+i);
+        }
+    } 
+}; 
+
+public class SyncPripiryTest {  
+    public static void main(String[] args) {  
+
+        System.out.println(Thread.currentThread().getName()
+                +"("+Thread.currentThread().getPriority()+ ")");
+
+        Thread t1=new MyThread1("t1");    // 新建t1
+        Thread t2=new MyThread1("t2");    // 新建t2
+        t1.setPriority(1);                // 设置t1的优先级为1
+        t2.setPriority(10);                // 设置t2的优先级为10
+        t1.setDaemon(true); //设置为守护进程
+        t1.start();                        // 启动t1
+        t2.start();                        // 启动t2
+    }  
+}
