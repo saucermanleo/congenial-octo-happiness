e5f5963cbd4915aba5417a25dc4f7796e8839982
zhangyang <38433673+saucermanleo@users.noreply.github.com>
2019-11-28 15:52:36 +0800

    bubblesort

---
+++ shujujiegou/src/main/java/com/zy/sort/BubbleSort.java
@@ -0,0 +1,31 @@
+package com.zy.sort;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ * 
+ * @author : 生态环境-张阳
+ * @date : 2019/11/28 0028 15:29
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arrays = {1, 4, 5, 3, 2, 9, 8, 7, 10, 11, 16, 14};
+        bubbleSort(arrays);
+        System.out.println(Arrays.toString(arrays));
+    }
+
+    private static void bubbleSort(int[] arrays) {
+        int length = arrays.length;
+        int temp;
+        for (int x = 0; x < length - 1; x++) {
+            for (int y = 0; y < length - x - 1; y++) {
+                if (arrays[y] > arrays[y + 1]) {
+                    temp = arrays[y + 1];
+                    arrays[y + 1] = arrays[y];
+                    arrays[y] = temp;
+                }
+            }
+        }
+    }
+}
